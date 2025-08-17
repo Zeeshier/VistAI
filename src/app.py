047a7ede11d31639f4bd1af7c351b59e94e9dd23
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-08-18 00:29:26 +0500

    add team route

--- src/app.py
+++ src/app.py
@@ -79,6 +79,10 @@ def results_page():
 
     return render_template("results.html")
 
+@app.route("/team", methods=["GET"])
+def team_page():
+    return render_template("team.html")
+
 if __name__ == "__main__":
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.run(debug=True)
